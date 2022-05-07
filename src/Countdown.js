748dcaec2d2e78c21b93b748919c678850205d85
Mateusz Marzęcki <mmarzecki@atlassian.com>
2022-05-07 14:00:48 +0200

    Implemented countdown component

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
+import './Countdown.css';
+
 class Countdown extends React.Component {
     constructor() {
         super();
 
         this.state = {
-
+            counter: 10,
         };
+
+        setInterval(() => {
+            this.setState({
+                counter: this.state.counter - 1,
+            })
+        }, 1000);
     }
 
     render() {
-        return '';
+        if (this.state.counter === -5) {
+            // it may not work if the window was opened by human
+            window.close();
+        }
+
+        return <div className="countdown">
+            {this.state.counter > 0
+                ? `Odliczanie: ${this.state.counter}`
+                : <div className="danger">H@CKED!!!</div>
+            }
+        </div>
     }
 }
 
